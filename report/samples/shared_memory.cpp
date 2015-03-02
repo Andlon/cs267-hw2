95a674a3c8ce5c9199c5dd6c68bfc0197b128589
Hang Su <suhang@icsi.berkeley.edu>
2015-03-02 11:55:12 -0800

    basic structure for report

---
+++ report/samples/shared_memory.cpp
@@ -0,0 +1,33 @@
+init particles
+init bins
+
+init locks
+
+foreach timestep t {
+  parallel {
+    foreach bin b {
+      clear (b);
+    }
+  }
+
+  parallel {
+    foreach particle i {
+      compute_bin_id b;
+      lock (b);
+      assign_to_bin (i, b);
+      unlock (b);
+    }
+  }
+
+  parallel {
+    foreach bin b {
+      compute_forces_in_bin (b);
+    }
+  }
+
+  parallel {
+    foreach particle i {
+      move (i);
+    }
+  }
+}
